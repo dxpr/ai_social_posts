500972f87b40d6e319155e90c968529557a3853c
Jurriaan Roelofs <jur@dxpr.com>
2024-11-26 11:56:56 +0100

    feat: make social post bundleable

--- src/Entity/SocialPost.php
+++ src/Entity/SocialPost.php
@@ -11,126 +11,44 @@ use Drupal\user\UserInterface;
 use Drupal\Core\Entity\EntityChangedTrait;
 
 /**
- * Defines the ContentEntityExample entity.
- *
- * @ingroup socials
- *
- * This is the main definition of the entity type. From it, an EntityType object
- * is derived. The most important properties in this example are listed below.
- *
- * id: The unique identifier of this entity type. It follows the pattern
- * 'moduleName_xyz' to avoid naming conflicts.
- *
- * label: Human readable name of the entity type.
- *
- * handlers: Handler classes are used for different tasks. You can use
- * standard handlers provided by Drupal or build your own, most probably derived
- * from the ones provided by Drupal. In detail:
- *
- * - view_builder: we use the standard controller to view an instance. It is
- *   called when a route lists an '_entity_view' default for the entity type.
- *   You can see this in the entity.socials_social_post.canonical
- *   route in the socials.routing.yml file. The view can be
- *   manipulated by using the standard Drupal tools in the settings.
- *
- * - list_builder: We derive our own list builder class from EntityListBuilder
- *   to control the presentation. If there is a view available for this entity
- *   from the views module, it overrides the list builder if the "collection"
- *   key in the links array in the Entity annotation below is changed to the
- *   path of the View. In this case the entity collection route will give the
- *   view path.
- *
- * - form: We derive our own forms to add functionality like additional fields,
- *   redirects etc. These forms are used when the route specifies an
- *   '_entity_form' or '_entity_create_access' for the entity type. Depending on
- *   the suffix (.add/.default/.delete) of the '_entity_form' default in the
- *   route, the form specified in the annotation is used. The suffix then also
- *   becomes the $operation parameter to the access handler. We use the
- *   '.default' suffix for all operations that are not 'delete'.
- *
- * - access: Our own access controller, where we determine access rights based
- *   on permissions.
- *
- * More properties:
- *
- *  - base_table: Define the name of the table used to store the data. Make sure
- *    it is unique. The schema is automatically determined from the
- *    BaseFieldDefinitions below. The table is automatically created during
- *    installation.
- *
- *  - entity_keys: How to access the fields. Specify fields from
- *    baseFieldDefinitions() which can be used as keys.
- *
- *  - links: Provide links to do standard tasks. The 'edit-form' and
- *    'delete-form' links are added to the list built by the
- *    entityListController. They will show up as action buttons in an additional
- *    column.
- *
- *  - field_ui_base_route: The route name used by Field UI to attach its
- *    management pages. Field UI module will attach its Manage Fields,
- *    Manage Display, and Manage Form Display tabs to this route.
- *
- * There are many more properties to be used in an entity type definition. For
- * a complete overview, please refer to the '\Drupal\Core\Entity\EntityType'
- * class definition.
- *
- * The following construct is the actual definition of the entity type which
- * is read and cached. Don't forget to clear cache after changes.
+ * Defines the Social Post entity.
  *
  * @ContentEntityType(
  *   id = "socials_social_post",
  *   label = @Translation("Social Post"),
+ *   bundle_label = @Translation("Social Post Type"),
+ *   base_table = "social_post",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "bundle" = "type"
+ *   },
  *   handlers = {
  *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
  *     "list_builder" = "Drupal\socials\Entity\Controller\SocialPostListBuilder",
  *     "form" = {
  *       "default" = "Drupal\socials\Form\SocialPostForm",
+ *       "add" = "Drupal\socials\Form\SocialPostForm",
+ *       "edit" = "Drupal\socials\Form\SocialPostForm",
  *       "delete" = "Drupal\socials\Form\SocialPostDeleteForm",
  *     },
  *     "access" = "Drupal\socials\SocialPostAccessControlHandler",
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
  *   },
- *   base_table = "social_post",
  *   admin_permission = "administer social post entity",
- *   entity_keys = {
- *     "id" = "id",
- *     "uuid" = "uuid",
- *   },
+ *   bundle_entity_type = "social_post_type",
+ *   field_ui_base_route = "entity.social_post_type.edit_form",
  *   links = {
  *     "canonical" = "/social_post/{socials_social_post}",
+ *     "add-page" = "/social_post/add",
+ *     "add-form" = "/social_post/add/{social_post_type}",
  *     "edit-form" = "/social_post/{socials_social_post}/edit",
  *     "delete-form" = "/social_post/{socials_social_post}/delete",
  *     "collection" = "/admin/content/social-posts"
- *   },
+ *   }
  * )
- *
- * The 'links' above are defined by their path. For core to find the
- * corresponding route, the route name must follow the correct pattern:
- *
- * entity.<entity_type>.<link_name>
- *
- * Example: 'entity.socials_social_post.canonical'.
- *
- * See the routing file at socials.routing.yml for the
- * corresponding implementation.
- *
- * The SocialPost class defines methods and fields for the social_post entity.
- *
- * Being derived from the ContentEntityBase class, we can override the methods
- * we want. In our case we want to provide access to the standard fields about
- * creation and changed time stamps.
- *
- * Our interface (see SocialPostInterface) also exposes the EntityOwnerInterface.
- * This allows us to provide methods for setting and providing ownership
- * information.
- *
- * The most important part is the definitions of the field properties for this
- * entity type. These are of the same type as fields added through the GUI, but
- * they can by changed in code. In the definition we can define if the user with
- * the rights privileges can influence the presentation (view, edit) of each
- * field.
- *
- * The class also uses the EntityChangedTrait trait which allows it to record
- * timestamps of save operations.
  */
 class SocialPost extends ContentEntityBase implements SocialPostInterface {
 
---
+++ src/Entity/SocialPostType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\socials\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Social Post Type configuration entity.
+ *
+ * @ConfigEntityType(
+ *   id = "social_post_type",
+ *   label = @Translation("Social Post Type"),
+ *   bundle_of = "socials_social_post",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   config_prefix = "social_post_type",
+ *   config_export = {
+ *     "id",
+ *     "label"
+ *   },
+ *   handlers = {
+ *     "list_builder" = "Drupal\socials\Entity\Controller\SocialPostTypeListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\socials\Form\SocialPostTypeForm",
+ *       "add" = "Drupal\socials\Form\SocialPostTypeForm",
+ *       "edit" = "Drupal\socials\Form\SocialPostTypeForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider"
+ *     },
+ *   },
+ *   admin_permission = "administer social post entity",
+ *   links = {
+ *     "canonical" = "/admin/structure/social_post_types/{social_post_type}",
+ *     "add-form" = "/admin/structure/social_post_types/add",
+ *     "edit-form" = "/admin/structure/social_post_types/{social_post_type}/edit",
+ *     "delete-form" = "/admin/structure/social_post_types/{social_post_type}/delete",
+ *     "collection" = "/admin/structure/social_post_types"
+ *   }
+ * )
+ */
+class SocialPostType extends ConfigEntityBundleBase {
+}
